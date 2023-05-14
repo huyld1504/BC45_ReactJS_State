06bb8ee0b0f80501521055a409d5c1b28264b204
ledinhhuy <ledinhhuy0105@gmail.com>
2023-05-14 12:53:52 +0700

    well done

--- bc_45_reactjs_state/src/App.js
+++ bc_45_reactjs_state/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import AppFeature from './apps/components/AppFeature';
 
 function App() {
--- bc_45_reactjs_state/src/apps/components/AppFeature.jsx
+++ bc_45_reactjs_state/src/apps/components/AppFeature.jsx
@@ -7,6 +7,7 @@ const listGlasses = [
     image: "./images/v1.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n1.jpg'
   },
   {
     id: 2,
@@ -15,6 +16,7 @@ const listGlasses = [
     image: "./images/v2.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n2.jpg'
   },
   {
     id: 3,
@@ -23,6 +25,7 @@ const listGlasses = [
     image: "./images/v3.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n3.jpg'
   },
   {
     id: 4,
@@ -31,6 +34,7 @@ const listGlasses = [
     image: "./images/v4.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n4.jpg'
   },
   {
     id: 5,
@@ -39,6 +43,7 @@ const listGlasses = [
     image: "./images/v5.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n5.jpg'
   },
   {
     id: 6,
@@ -47,6 +52,7 @@ const listGlasses = [
     image: "./images/v6.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n6.jpg'
   },
   {
     id: 7,
@@ -55,6 +61,7 @@ const listGlasses = [
     image: "./images/v7.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n7.jpg'
   },
   {
     id: 8,
@@ -63,6 +70,7 @@ const listGlasses = [
     image: "./images/v8.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n8.jpg'
   },
   {
     id: 9,
@@ -71,6 +79,7 @@ const listGlasses = [
     image: "./images/v9.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n9.jpg'
   },
 ];
 export default class AppFeature extends Component {
@@ -81,6 +90,7 @@ export default class AppFeature extends Component {
     image: "./images/v9.png",
     decription:
       "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    detail: './images/n9.jpg'
   };
 
   renderListproduct = () => {
@@ -95,15 +105,23 @@ export default class AppFeature extends Component {
           <div className="card-body">
             <img src={glass.image} alt="" style={{ width: "100%" }} />
             <h4 className="mt-2">Price:{glass.price} $</h4>
-            <button className="btn btn-primary mt-3 mx-auto d-block text-center mx-auto mb-2" onclick={() => {
-                // this.setState({
-                //   name: glass.name,
-                //   price: glass.price,
-                //   image: glass.image,
-                //   decription: glass.decription,
-                // });
-                console.log(123)
-              }}>View detail</button>
+            <a href="#product_detail">
+            <button
+              className="btn btn-primary mt-3 mx-auto d-block text-center mx-auto mb-2"
+              onClick={() => {
+                this.setState({
+                  name: glass.name,
+                  price: glass.price,
+                  image: glass.image,
+                  decription: glass.decription,
+                  detail: glass.detail
+                });
+                
+              }}
+            >
+              View detail
+            </button>
+            </a>
           </div>
         </div>
       );
@@ -120,26 +138,17 @@ export default class AppFeature extends Component {
               <div className="row">{this.renderListproduct()}</div>
             </div>
             <div className="col-4 mt-4">
-              <img className="model_glasses" src="../images/model.jpg" alt="" />
-              <img
-                className="glasses"
-                style={{ width: 250 }}
-                src={this.state.image}
-                alt=""
-              />
-              <br />
-              <h3>
-                Tên sản phẩm:{" "}
-                <span className="text-success">{this.state.name}</span>
-              </h3>
-              <h3>
-                Giá: <span className="text-success">{this.state.price}$</span>
-              </h3>
-              <h3>
-                Mô tả sản phẩm:{" "}
-                <span className="text-success">{this.state.decription}</span>
-              </h3>
+              <img id="product_detail" src={this.state.detail} alt="..." style={{width:'100%'}} />
+              {/* <img src={this.state.image} alt="" /> */}
+              <div className="card">
+              <h3 className="text-center">Product name: <span className="fs-5 text-danger fw-bold">{this.state.name}</span></h3>
+              <div className="card-body">
+                <p className="fs-3">Price: <span className="fs-5 text-danger fw-bold">{this.state.price} $</span></p>
+                <p className="fs-3">Decription: <span className="fs-5 text-danger fw-bold">{this.state.decription} </span></p>
+              </div>
             </div>
+            </div>
+            
           </div>
         </div>
       </div>
